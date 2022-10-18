9f82786065f53c507ff7630e144a6af5c97b6964
ice <ice@ubuntu.myguest.virtualbox.org>
2022-10-18 15:07:40 -0300

    fix: ReactDOM import

--- hello/src/index.js
+++ hello/src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 
 function HelloWorld(){
     return(
         <div>
             <h1>Hello World</h1>
         </div>
-
     );
 };
 
 ReactDOM.render(
     <HelloWorld />,
-    document.getElementById(root)
+    document.getElementById('root')
 );
